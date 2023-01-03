221a61d5d01bbfa34ce21cf36f46d3173f3836aa
GerryBastian <117911374+GerryBastian@users.noreply.github.com>
2023-01-03 15:02:51 +0700

    Add files via upload

---
+++ Soal1.py
@@ -0,0 +1,19 @@
+print("========================================")
+print("************* Justice League ***********")
+print("========================================")
+
+brucewayne = 1
+victorstone = 2
+ciscoramon= 3
+name = str,brucewayne , victorstone , ciscoramon
+a = str(input("Masukan username anda:"))
+if a != name :  
+ print("===== WELCOME ",str (a),"======")
+ print("1. Tambah Anggota Justice League")
+ print("2. Hapus Anggota Justive League")
+ print("3. Tampilkan Anggota Justice League")
+ print("4. Exit ")
+
+
+
+
---
+++ Soal2.py
@@ -0,0 +1,20 @@
+print("===== Selamat datang di XXV ====")
+a=int(input("Masukan tanggal hari ini: "))
+
+print("== Berikut genre film yang tersedia ==")
+print("1. Horror")
+print("2. Action")
+
+b =int(input("Silahkan mau nonto film apa : "))
+if b== 1:
+ print("== Berikut pilihan film  yang horror sedang tayang ==")
+ print("1. The Devil's Light")
+ print("2. Pengabdi Setan ")
+elif b== 2:
+ print("== Berikut pilihan film Action yang  sedang tayang ==")
+ print("1. Black panther: Wakanda Forever")
+ print("2. Avatar: The Way of Water ")
+
+c=int(input("Silahkan pilih mau nonton film apa : "))
+d=int(input("Mau memesan tiket sebanyak : "))
+print("Total yang harus dibayar adalah Rp.  237500")
